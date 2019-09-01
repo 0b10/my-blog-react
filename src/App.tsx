ccaf260a5d5643c288d8a4c0c0679853f48fdcde
0b10 <0b10@pm.me>
2019-09-01 20:49:30 +0100

    Remove inline func expressions from AppRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,32 @@
 import React from "react";
 
-import { Route, Switch } from "react-router-dom";
+import { Route, RouteComponentProps, Switch } from "react-router-dom";
+import { StaticContext } from "react-router";
 
 import { Posts, PostContent } from "./views";
 import { withPostContentQuery, withPostsQuery } from "./graphql";
 
+// >>> INIT >>>
 const PostContentQuery = withPostContentQuery(PostContent);
 const PostsQuery = withPostsQuery(Posts);
+
+// >>> RENDERER WRAPPERS >>>
+const PostsQueryRenderer = ({ history }: RouteComponentProps<any, StaticContext, any>) => (
+  <PostsQuery routeHandler={id => history.push(`/post/${id}`)} />
+);
+
+const PostContentQueryRenderer = ({ match }: RouteComponentProps<any, StaticContext, any>) => (
+  <PostContentQuery postId={match.params.id} />
+);
+
+// >>> COMPONENTS >>>
+export const AppRouter: React.FC = () => (
+  <Switch>
+    <Route path="/" exact render={PostsQueryRenderer} />} />
+    <Route path="/post/:id" exact render={PostContentQueryRenderer} />
+  </Switch>
+);
 
 export const App: React.FC = () => {
   return <AppRouter />;
 };
-
-export const AppRouter: React.FC = () => (
-  <Switch>
-    <Route
-      path="/"
-      exact
-      render={props => <PostsQuery routeHandler={id => props.history.push(`/post/${id}`)} />}
-    />
-    <Route
-      path="/post/:id"
-      exact
-      render={props => <PostContentQuery postId={props.match.params.id} />}
-    />
-  </Switch>
-);
