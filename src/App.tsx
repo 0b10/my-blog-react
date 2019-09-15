bcb24555d6c4aac6ef4f10e880c4ca64becc626a
0b10 <0b10@pm.me>
2019-09-15 05:36:49 +0100

    Rename route adapters

--- src/App.tsx
+++ src/App.tsx
@@ -11,24 +11,24 @@ const PostContentQuery = withPostContentQuery(PostContent);
 const PostsQuery = withPostsQuery(Posts);
 
 // >>> RENDERER WRAPPERS >>>
-const PostsQueryRenderer = ({ history }: RouteComponentProps<any, StaticContext, any>) => (
+const PostsQueryAdapter = ({ history }: RouteComponentProps<any, StaticContext, any>) => (
   <PostsQuery routeHandler={(id) => history.push(`/post/${id}`)} />
 );
 
-const PostContentQueryRenderer = ({ match }: RouteComponentProps<any, StaticContext, any>) => (
+const PostContentQueryAdapter = ({ match }: RouteComponentProps<any, StaticContext, any>) => (
   <PostContentQuery postId={match.params.id} />
 );
 
-const NavBarRenderer = ({ history }: RouteComponentProps<any, StaticContext, any>) => (
+const NavBarAdapter = ({ history }: RouteComponentProps<any, StaticContext, any>) => (
   <NavBar items={[{ path: "/", text: "home" }]} routeHandler={history.push} />
 );
 
 // >>> COMPONENTS >>>
 export const AppRouter: React.FC = () => (
   <React.Fragment>
-    <Route path="/" render={NavBarRenderer} />
-    <Route path="/" exact render={PostsQueryRenderer} />
-    <Route path="/post/:id" exact render={PostContentQueryRenderer} />
+    <Route path="/" render={NavBarAdapter} />
+    <Route path="/" exact render={PostsQueryAdapter} />
+    <Route path="/post/:id" exact render={PostContentQueryAdapter} />
   </React.Fragment>
 );
 
