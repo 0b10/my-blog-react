0d9d83061c2103301145fb58a10505368cd9301d
0b10 <0b10@pm.me>
2019-09-16 07:14:44 +0100

    Test SubmitPost reset event
      + Make the SubmitPost/Preview reset test pass
      + Test that no validation message is displayed after a reset

--- src/views/SubmitPost/__tests__/integration/reset.test.tsx
+++ src/views/SubmitPost/__tests__/integration/reset.test.tsx
@@ -1,9 +1,9 @@
 import "@testing-library/jest-dom/extend-expect";
 
-import { fireEvent, within } from "@testing-library/react";
+import { fireEvent, waitForDomChange, within } from "@testing-library/react";
 
-import { labels, renderSubmitPost, testids } from "../helpers";
 import { ISubmitPostInputFields } from "../../Form";
+import { labels, renderSubmitPost, testids } from "../helpers";
 
 describe("integration tests: SubmitPost", () => {
   describe("onReset [#event,#form]", () => {
@@ -11,6 +11,7 @@ describe("integration tests: SubmitPost", () => {
       readonly input: string;
       readonly labelRegex: RegExp;
       readonly name: keyof ISubmitPostInputFields<string>;
+      readonly validationMsgTestId: string;
     }
 
     const testCases: ISubmitPostInputFields<IOnResetTestData> = Object.freeze({
@@ -18,35 +19,57 @@ describe("integration tests: SubmitPost", () => {
         input: "test title",
         labelRegex: labels.reTitle,
         name: "title",
+        validationMsgTestId: testids.validation.message.title,
       },
       tldr: {
         input: "test tldr",
         labelRegex: labels.reTldr,
         name: "tldr",
+        validationMsgTestId: testids.validation.message.tldr,
       },
       body: {
         input: "test body",
         labelRegex: labels.reBody,
         name: "body",
+        validationMsgTestId: testids.validation.message.body,
       },
     });
 
-    Object.values(testCases).forEach(({ name, input, labelRegex }: IOnResetTestData) => {
-      describe(`for the ${name} field`, () => {
-        // TODO: [#test] also test that the error message fields are also reset
-        it("should not render in the preview field", async () => {
-          // ! BUG(FIXED) jsdom 15.1.1 required: jsdom/jsdom:#2387 and sharegate/craco#108
-          const reInput = RegExp(`^${input}$`);
-          const result = renderSubmitPost();
+    Object.values(testCases).forEach(
+      ({ name, input, labelRegex, validationMsgTestId }: IOnResetTestData) => {
+        describe(`for the ${name} field`, () => {
+          it("should not render anything in the corresponding input in the preview field", async () => {
+            // ! BUG(FIXED) jsdom 15.1.1 required: jsdom/jsdom:#2387 and sharegate/craco#108
+            const reInput = RegExp(`^${input}$`);
+            const result = renderSubmitPost();
+            const inputField = result.getByLabelText(labelRegex);
+            const preview = result.getByTestId(testids.preview.wrapper);
+            const resetButton = result.getByText(/^Reset$/);
 
-          fireEvent.change(result.getByLabelText(labelRegex), { target: { value: input } });
-          const preview = result.getByTestId(testids.preview.wrapper);
-          expect(await within(preview).findByText(reInput)).toBeVisible(); // defensive, check text exists first
-          fireEvent.click(result.getByText(/^Reset$/));
+            fireEvent.change(inputField, { target: { value: input } });
+            expect(await within(preview).findByText(reInput)).toBeVisible(); // defensive, check text exists first
+            fireEvent.click(resetButton);
 
-          expect(preview).not.toHaveTextContent(reInput);
+            expect(preview).not.toHaveTextContent(reInput);
+          });
+
+          it("should not render anything in the corresponding validation error message field", async () => {
+            const result = renderSubmitPost();
+            const errorField = result.getByTestId(validationMsgTestId);
+            const inputField = result.getByLabelText(labelRegex);
+            const resetButton = result.getByText(/^Reset$/);
+
+            // focus => blur === `touched` && "Required" message
+            fireEvent.focus(inputField);
+            fireEvent.blur(inputField);
+            await waitForDomChange({ container: inputField });
+            expect(errorField).toHaveTextContent(/.+/); // Check that a message exists first
+            fireEvent.click(resetButton);
+
+            expect(errorField).toHaveTextContent(/^$/);
+          });
         });
-      });
-    });
+      }
+    );
   });
 });
